bbdf3e5a9e6695ac922f7cc27299774154bc358a
ayulestari <ayu339395@gmail.com>
2021-03-02 07:11:20 -0800

    belajar php

---
+++ baco.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Belanja</title>
+</head>
+<body>
+    <form action="" method="">
+        <label for="">Customer</label>
+        <input type="text" name="customer" value="">
+        <br>
+        
+        <label for="">Nama Barang</label>
+        <label for="">TV</label>
+        <input type="radio" name="nama_barang" value="TV">
+        <label for="">Kulkas</label>
+        <input type="radio" name="nama_barang" value="Kulkas">
+        <label for="">Mesin Cuci</label>
+        <input type="radio" name="nama_barang" value="Mesin Cuci">
+        <br>
+        <label for="" name="jumlah_beli">Jumlah barang beli</label>
+        <input type="text" name="jumlah_beli">
+        <br>
+        <input type="submit" name="simpan">
+    </form>
+</body>
+</html>
